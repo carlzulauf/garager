0ab265cdf97a43bcd081582570a5425f0cd65dc0
Carl Zulauf <carl@linkleaf.com>
2016-04-24 22:18:59 -0500

    Throttle frequent garage toggles (or attempt to)

--- lib/garager/garage.rb
+++ lib/garager/garage.rb
@@ -1,11 +1,12 @@
 module Garager
   class Garage
-    attr_accessor :pin, :presumed, :logger
+    attr_accessor :pin, :presumed, :logger, :last
 
     def initialize(options = {})
       self.pin      = options.fetch(:pin, 4)
       self.presumed = options.fetch(:presumed, :closed)
       self.logger   = options.fetch(:logger){ Logger.new(STDOUT) }
+      self.last     = 0.0
     end
 
     def opened?
@@ -27,13 +28,23 @@ module Garager
       self
     end
 
+    def max_frequency
+      3.0 # seconds
+    end
+
     def toggle
-      logger.info "Garage#toggle. Presumed #{presumed}"
-      gpio "write #{pin} 0"
-      sleep 0.5
-      gpio "write #{pin} 1"
-      self.presumed = presumed == :closed ? :open : :closed
-      self
+      ts = Time.now.to_f
+      if ts - last > max_frequency
+        logger.info "Garage#toggle. Presumed #{presumed}"
+        gpio "write #{pin} 0"
+        sleep 0.5
+        gpio "write #{pin} 1"
+        self.presumed = presumed == :closed ? :open : :closed
+        self.last = ts
+        self
+      else
+        logger.info "Garage#toggle. Throttled."
+      end
     end
 
     def gpio(cmd)
