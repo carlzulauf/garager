c866ddc09445d996787a7453a4f63890f60267a6
Carl Zulauf <carl@linkleaf.com>
2022-12-31 14:08:00 -0700

    Update to env and spec suite to get them to play nice

--- spec/garager_spec.rb
+++ spec/garager_spec.rb
@@ -1,4 +1,4 @@
-describe "Garager" do
+RSpec.describe "Garager" do
   it "should exist as a module" do
     expect(Garager).to be_a(Module)
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,28 @@
+# stdlib stuff
+require "securerandom"
+require "logger"
+require "yaml"
+require "open3"
+
+# o the gems
 require "bundler/setup"
+Bundler.require(:default)
 require "pry"
+
+GARAGER_ENV = "test"
+ROOT_DIR    = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+CONFIG_DIR  = File.join(ROOT_DIR, "config")
+CONFIG_PATH = File.join(CONFIG_DIR, "#{GARAGER_ENV}.yaml")
+LIB_DIR     = File.join(ROOT_DIR, "lib")
+
+LOG_DIR = File.join(ROOT_DIR, "log")
+FileUtils.mkdir(LOG_DIR) unless File.exist?(LOG_DIR)
+LOGGER = Logger.new(File.join(LOG_DIR, "test.log"))
+
 require "garager"
+
+Garager::ConfigBuilder.out(GARAGER_ENV) unless File.exist?(CONFIG_PATH)
+CONFIG_DATA = YAML.load_file(CONFIG_PATH)
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
