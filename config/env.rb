9ab00a84ddeefd94cccada9171e58718f5e82f97
Carl Zulauf <carl@linkleaf.com>
2023-02-18 15:54:53 -0700

    Default to info log level. Set to debug based on config.

--- config/env.rb
+++ config/env.rb
@@ -16,13 +16,15 @@ CONFIG_PATH = File.join(CONFIG_DIR, "#{GARAGER_ENV}.yaml")
 ROOT_DIR    = File.expand_path(File.join(CONFIG_DIR, ".."))
 LIB_DIR     = File.join(ROOT_DIR, "lib")
 LOGGER      = Logger.new(STDOUT)
+LOGGER.level = :info
 
 $LOAD_PATH.unshift(LIB_DIR) unless $LOAD_PATH.member?(LIB_DIR)
 require "garager"
 
 Garager::ConfigBuilder.out(GARAGER_ENV) unless File.exist?(CONFIG_PATH)
 CONFIG_DATA = YAML.load_file(CONFIG_PATH)
+LOGGER.level = :debug if CONFIG_DATA["debug"]
 
 Dir.chdir(ROOT_DIR) unless Dir.pwd == ROOT_DIR
 
-LOGGER.info "Starting with config: " + CONFIG_DATA.inspect
+LOGGER.debug "Starting with config: " + CONFIG_DATA.inspect
